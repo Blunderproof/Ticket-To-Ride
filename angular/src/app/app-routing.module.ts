e3e120d09705f61fecee97006065d2f2914df004
delashum <delashum@gmail.com>
2018-02-05 15:30:13 -0700

    Places assets in proper place, moved styles, connected router.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { GamelobbyComponent } from './gamelobby/gamelobby.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'lobby',
+  component: GamelobbyComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
