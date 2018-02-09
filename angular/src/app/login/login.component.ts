537f89656ef0c9b775cbe6e8d831ebb2e2abc5fd
Kody Davis <kodycd09@byu.local>
2018-02-08 20:37:52 -0700

    Separated controls for Register and Login, added methods in server_proxy for login and register

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -9,15 +9,40 @@ import { createFormGroup } from '../core/utils/forms';
 })
 export class LoginComponent implements OnInit {
 
-  controls = {
-    username: this._fb.control('', Validators.required)
+  registering = false;
+
+  login_controls = {
+    username: this._fb.control('', Validators.required),
+    password: this._fb.control('', Validators.required)
   };
 
-  form = createFormGroup(this.controls);
+  register_controls = {
+    username: this._fb.control('', Validators.required),
+    password: this._fb.control('', Validators.required),
+    confirmPassword: this._fb.control('', Validators.required)
+  };
+  form = createFormGroup(this.register_controls);
+
+  setRegistering(value: boolean) {
+    this.registering = value;
+    if (this.registering) {
+      this.form = createFormGroup(this.register_controls);
+    } else {
+      this.form = createFormGroup(this.login_controls);
+    }
+  }
 
   constructor(private _fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  onSubmit() {
+    if (!this.registering) {
+      alert(`Username: ${this.login_controls.username.value}, Password: ${this.login_controls.password.value}`);
+    } else {
+      alert(`Username: ${this.register_controls.username.value}, Password: ${this.register_controls.password.value},
+       Confirm: ${this.register_controls.confirmPassword.value}`);
+    }
   }
 
 }
--- angular/src/app/services/server_proxy.service.ts
+++ angular/src/app/services/server_proxy.service.ts
@@ -17,4 +17,13 @@ export class ServerProxy {
     joinGame() {
         this.communicator.send('joinGame', {});
     }
+
+    login(username: String, password: String) {
+        this.communicator.send('login', {'username': username, 'password': password }); // needs to include username and password
+    }
+
+    register(username: String, password: String, confirmPassword: String) {
+        // tslint:disable-next-line:max-line-length
+        this.communicator.send('register', {'username': username, 'password': password, 'confirmPassword': confirmPassword}); // needs to include username password and confirmPassword
+    }
 }
