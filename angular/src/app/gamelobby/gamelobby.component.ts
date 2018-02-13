1f9e36457c14e21484bb9958482c5c28198f04bc
Jay Davis <jdavis@dhicorp.com>
2018-02-12 22:25:00 -0700

    Minor style changes Buttons disable when appropriate Fixed Join Game button show/hide logic Added Error Messages

--- angular/src/app/gamelobby/gamelobby.component.ts
+++ angular/src/app/gamelobby/gamelobby.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./gamelobby.component.scss']
 })
 export class GameLobbyComponent implements OnInit {
+  errorMessages = [];
 
   constructor(public _playerInfo: PlayerInfo, private communicator: ServerProxy, private _router: Router) { }
 
@@ -16,31 +17,43 @@ export class GameLobbyComponent implements OnInit {
   }
 
   startGame() {
+    this.errorMessages = [];
     this.communicator.startGame()
       .then((x: any) => {
         // TODO: Don't navigate until socket emits to start game
         if (x.success) {
+          this.errorMessages = [];
           this._router.navigate(['/game']);
+        } else {
+          this.errorMessages.push(x.message);
         }
       });
   }
 
   leaveGame() {
+    this.errorMessages = [];
     this.communicator.leaveGame()
       .then((x: any) => {
         // TODO: Don't navigate until socket emits to start game
         if (x.success) {
+          this.errorMessages = [];
           this._router.navigate(['/lobby']);
+        } else {
+          this.errorMessages.push(x.message);
         }
       });
   }
 
   deleteGame() {
+    this.errorMessages = [];
     this.communicator.deleteGame()
       .then((x: any) => {
         // TODO: Don't navigate until socket emits to start game
         if (x.success) {
+          this.errorMessages = [];
           this._router.navigate(['/lobby']);
+        } else {
+          this.errorMessages.push(x.message);
         }
       });
   }
