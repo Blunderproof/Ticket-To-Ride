c9446f069f09e41315c3ba2cb1a21796304b1cdc
delashum <delashum@gmail.com>
2018-02-12 23:05:10 -0700

    Lsat commit of phase 1 for jacob

--- modules/commands/CommandResults.ts
+++ modules/commands/CommandResults.ts
@@ -3,14 +3,14 @@ export default class CommandResults {
   private data: any;
   private errorInfo: string;
   private userCookie: string;
-  private emitRequest: any;
+  private emitRequests: any[];
 
   constructor(responseData: any) {
     this.success = responseData.success;
     this.data = responseData.data;
     this.errorInfo = responseData.errorInfo;
     this.userCookie = responseData.userCookie;
-    this.emitRequest = responseData.emit;
+    this.emitRequests = responseData.emit;
   }
 
   public getData(): any {
@@ -25,7 +25,7 @@ export default class CommandResults {
   public shouldSetSession(): string {
     return this.userCookie;
   }
-  public shouldEmit(): any {
-    return this.emitRequest;
+  public shouldEmit(): any[] {
+    return this.emitRequests;
   }
 }
