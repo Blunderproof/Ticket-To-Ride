33d0990d044b1d3c59d2caa25ce8639b2e886d21
delashum <delashum@gmail.com>
2018-02-26 17:20:31 -0700

    Add destination_cards.csv

--- models/DestinationCard.ts
+++ models/DestinationCard.ts
@@ -7,12 +7,22 @@ export interface IDestinationCardModel extends mongoose.Document {
   pointValue: number
 }
 
-
 export var DestinationCardSchema: Schema = new Schema({
   city1: String,
   city2: String,
   pointValue: Number
 });
+
+DestinationCardSchema.methods.isCompletedFromPath = function() {
+    //pass
+};
+
+
+DestinationCardSchema.virtual('fullID').get(function(this: IDestinationCardModel) {
+    return [this.city1, this.city2].join('-')
+});
+
+
 
 export const DestinationCard: mongoose.Model<IDestinationCardModel> = mongoose.model<IDestinationCardModel>(
    "DestinationCard",
