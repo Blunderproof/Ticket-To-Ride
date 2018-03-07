225b4902ce0bdfea8bfd5cae6d430d89bfda577b
Kody Davis <kodycd09@byu.local>
2018-03-07 16:01:14 -0700

    Game History should work be functional on the front end.

--- angular/src/app/game-history/game-history.component.ts
+++ angular/src/app/game-history/game-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameHistory } from '../services/game-history.service';
+import { SocketCommunicator } from '../services/socket_communicator.service';
 
 @Component({
   selector: 'app-game-history',
@@ -9,10 +10,19 @@ import { GameHistory } from '../services/game-history.service';
 export class GameHistoryComponent implements OnInit {
 
   // https://www.youtube.com/watch?v=I317BhehZKM&t=37s
+  historyList = [];
 
-  constructor(public _gameHistory: GameHistory) { }
+  constructor(public _gameHistory: GameHistory, private socket: SocketCommunicator) { }
 
   ngOnInit() {
+    this.sockets();
+  }
+
+  sockets() {
+    this.socket.updateChatHistory(data => {
+      console.log(data);
+      this.historyList = data;
+    });
   }
 
 }
