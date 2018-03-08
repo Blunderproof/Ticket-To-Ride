99be1fb491a8e673db5bb201777d7281ed9f8802
Clay Coleman <coleclayman@gmail.com>
2018-03-08 16:14:00 -0700

    changes to game to add test suite, necessary changes in message and user to accommodate test suite

--- angular/src/app/classes/user.ts
+++ angular/src/app/classes/user.ts
@@ -4,19 +4,25 @@ import { DestinationCard } from './destination-card';
 import { PlayerColor } from './constants';
 
 export class User {
-    _id: string;
-    username: string;
-    claimedRouteList: Route[];
-    trainCardHand: TrainCard[];
-    destinationCardHand: DestinationCard[];
-    score: number;
-    color: PlayerColor;
+  _id: string;
+  username: string;
+  claimedRouteList: Route[];
+  trainCardHand: TrainCard[];
+  destinationCardHand: DestinationCard[];
+  score: number;
+  tokenCount: number;
+  color: PlayerColor;
+  trainCardCount: any;
 
-    constructor(data?: Object) {
-        Object.keys(data || {}).forEach(key => this[key] = data[key]);
+  constructor(data?: Object) {
+    Object.keys(data || {}).forEach(key => (this[key] = data[key]));
 
-        this.claimedRouteList = (this.claimedRouteList || []).map(x => new Route(x));
-        this.trainCardHand = (this.trainCardHand || []).map(x => new TrainCard(x));
-        this.destinationCardHand = (this.destinationCardHand || []).map(x => new DestinationCard(x));
-    }
+    this.claimedRouteList = (this.claimedRouteList || []).map(
+      x => new Route(x)
+    );
+    this.trainCardHand = (this.trainCardHand || []).map(x => new TrainCard(x));
+    this.destinationCardHand = (this.destinationCardHand || []).map(
+      x => new DestinationCard(x)
+    );
+  }
 }
--- angular/src/app/game/game.component.ts
+++ angular/src/app/game/game.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { GameHistory } from '../services/game-history.service';
 import { UserInfo } from '../services/user_info.service';
 import { User } from '../classes/user';
+import { Message } from '../classes/message';
+import { MessageType } from '../classes/constants';
+import { ServerProxy } from '../services/server_proxy.service';
+import { GameHistory } from '../services/game-history.service';
 
 @Component({
   selector: 'app-game',
@@ -12,7 +15,13 @@ export class GameComponent implements OnInit {
   historyOpen = true;
   gameStart = false;
 
-  constructor(public _gameHistory: GameHistory, public _userInfo: UserInfo) {}
+  simState = 0;
+
+  constructor(
+    private _serverProxy: ServerProxy,
+    public _gameHistory: GameHistory,
+    public _userInfo: UserInfo
+  ) {}
 
   ngOnInit() {
     this._userInfo.getUser();
@@ -24,5 +33,154 @@ export class GameComponent implements OnInit {
     console.log(this._userInfo);
 
     console.log('YES');
+  }
+
+  simulateChanges() {
+    let index = 0;
+    let cardIndex = 0;
+    switch (this.simState) {
+      case 0:
+        alert('Changing player points.');
+        index = Math.floor(Math.random() * this._userInfo.game.userList.length);
+        let score = Math.floor(Math.random() * 50);
+        this._userInfo.game.userList[index].score = score;
+
+        alert(
+          `Changed player ${
+            this._userInfo.game.userList[index].username
+          }'s points to ${score}.`
+        );
+        break;
+      case 1:
+        alert(`Changing my number of train cards and destination cards.`);
+        for (index = 0; index < this._userInfo.game.userList.length; index++) {
+          const element = this._userInfo.game.userList[index];
+          if (element._id == this._userInfo.user._id) {
+            break;
+          }
+        }
+        for (let i = 0; i < 3; i++) {
+          cardIndex = Math.floor(
+            Math.random() * this._userInfo.game.trainCardDeck.length
+          );
+          this._userInfo.user.trainCardCount[
+            this._userInfo.game.trainCardDeck[cardIndex].color
+          ] += 1;
+
+          this._userInfo.game.userList[index].trainCardHand.push(
+            this._userInfo.game.trainCardDeck[cardIndex]
+          );
+        }
+
+        for (let i = 0; i < 2; i++) {
+          cardIndex = Math.floor(
+            Math.random() * this._userInfo.game.destinationCardDeck.length
+          );
+          this._userInfo.game.userList[index].destinationCardHand.push(
+            this._userInfo.game.destinationCardDeck[cardIndex]
+          );
+        }
+        alert(
+          `Changed my train card hand to ${
+            this._userInfo.game.userList[index].trainCardHand.length
+          } cards and destination card to ${
+            this._userInfo.game.userList[index].destinationCardHand.length
+          }.`
+        );
+        break;
+      case 2:
+        alert(
+          `Now changing other user's number of train cards, destination cards, and train car tokens.`
+        );
+
+        while (true) {
+          index = Math.floor(
+            Math.random() * this._userInfo.game.userList.length
+          );
+          if (
+            this._userInfo.game.userList[index]._id != this._userInfo.user._id
+          )
+            break;
+        }
+
+        for (let i = 0; i < 3; i++) {
+          cardIndex = Math.floor(
+            Math.random() * this._userInfo.game.trainCardDeck.length
+          );
+          this._userInfo.game.userList[index].trainCardHand.push(
+            this._userInfo.game.trainCardDeck[cardIndex]
+          );
+        }
+
+        for (let i = 0; i < 4; i++) {
+          cardIndex = Math.floor(
+            Math.random() * this._userInfo.game.destinationCardDeck.length
+          );
+          this._userInfo.game.userList[index].destinationCardHand.push(
+            this._userInfo.game.destinationCardDeck[cardIndex]
+          );
+        }
+
+        this._userInfo.game.userList[index].tokenCount = Math.floor(
+          Math.random() * 30
+        );
+        alert(
+          `Changed player ${
+            this._userInfo.game.userList[index].username
+          }'s trainCards to ${
+            this._userInfo.game.userList[index].trainCardHand.length
+          } and their destination cards to ${
+            this._userInfo.game.userList[index].destinationCardHand.length
+          } and their train car tokens to ${
+            this._userInfo.game.userList[index].tokenCount
+          }.`
+        );
+
+        break;
+      case 3:
+        alert(
+          `Changing the number of train cards and destination cards in the decks.`
+        );
+
+        let destCountToDelete = Math.floor(
+          Math.random() * this._userInfo.game.destinationCardDeck.length / 2
+        );
+        this._userInfo.game.destinationCardDeck.splice(0, destCountToDelete);
+
+        let trainCountToDelete = Math.floor(
+          Math.random() * this._userInfo.game.trainCardDeck.length / 2
+        );
+        this._userInfo.game.trainCardDeck.splice(6, trainCountToDelete);
+        alert(
+          `Removed ${destCountToDelete} destination cards and ${trainCountToDelete} train cards.`
+        );
+        break;
+      case 4:
+        alert('Use the claim button below to add a claimed route');
+
+        break;
+      case 5:
+        alert('Adding chat message.');
+
+        this._serverProxy.sendMessage('test messsage sent!!').then((x: any) => {
+          alert('Chat message sent.');
+        });
+
+        break;
+      case 6:
+        alert(
+          'Note how gameHistory was changed upon selecting the destination cards.'
+        );
+
+        break;
+
+      default:
+        break;
+    }
+
+    this.simState += 1;
+    // Add claimed route (for any player)
+    // Add chat message from any player
+    // Add game history entries
   }
 }
