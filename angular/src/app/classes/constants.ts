370bc8b2ef5bf999435f3f4d3f7f1bfbfb049631
Jay Davis <jay_s_davis@hotmail.com>
2018-03-15 19:54:10 -0600

    Opponent card updates

--- angular/src/app/classes/constants.ts
+++ angular/src/app/classes/constants.ts
@@ -19,6 +19,14 @@ export enum PlayerColor {
     White = 'white'
 }
 
+export enum RGBAColor {
+    red = 'rgba(200, 80, 80, .8)',
+    green = 'rgba(80, 200, 80, .8)',
+    blue = 'rgba(0, 80, 200, .8)',
+    black = 'rgba(80, 80, 80, .8)',
+    white = 'rgba(240, 240, 240, .9)'
+}
+
 export enum MessageType {
     Chat = 'chat',
     History = 'history'
--- angular/src/app/game/game.component.ts
+++ angular/src/app/game/game.component.ts
@@ -14,8 +14,8 @@ import { GameHistory } from '../services/game-history.service';
 export class GameComponent implements OnInit {
   historyOpen = true;
   gameStart = false;
-  
-  gameOver = true;
+
+  gameOver = false;
 
   simState = 0;
 
