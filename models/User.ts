9f04e6dfecf211f28ea47cc4a6b16e7d7424825c
delashum <delashum@gmail.com>
2018-03-21 16:20:55 -0600

    intermediary changes

--- models/User.ts
+++ models/User.ts
@@ -6,104 +6,105 @@ import { PlayerColor } from '../constants';
 import { Schema } from 'mongoose';
 
 export interface IUser {
-	username: string;
-	hashedPassword: string;
-	claimedRouteList: IRouteModel[];
-	trainCardHand: ITrainCardModel[];
-	destinationCardHand: IDestinationCardModel[];
-	points: {
-		public: number;
-		private: number;
-		detailed: {
-			routes: number;
-			destinationCardPositive: number;
-			destinationCardNegative: number;
-			longestRoute: number;
-		};
-	};
-	score: number;
-	tokenCount: number;
-	color: PlayerColor;
+  username: string;
+  hashedPassword: string;
+  claimedRouteList: IRouteModel[];
+  trainCardHand: ITrainCardModel[];
+  destinationCardHand: IDestinationCardModel[];
+  score: number;
+  tokenCount: number;
+  color: PlayerColor;
+
+  publicPoints: Promise<any>;
+  privatePoints: Promise<any>;
+  routePoints: Promise<any>;
+  longestRoutePoints: Promise<any>;
+  destinationCardNegativePoints: Promise<any>;
+  destinationCardPositivePoints: Promise<any>;
 }
 
 export interface IUserModel extends IUser, mongoose.Document {}
 
 export var UserSchema: mongoose.Schema = new mongoose.Schema(
-	{
-		username: { type: String, required: true, index: { unique: true } },
-		hashedPassword: { type: String, required: true },
-		claimedRouteList: [{ type: Schema.Types.ObjectId, ref: 'Route' }],
-		trainCardHand: [{ type: Schema.Types.ObjectId, ref: 'TrainCard' }],
-		destinationCardHand: [
-			{ type: Schema.Types.ObjectId, ref: 'DestinationCard' }
-		],
-		score: Number,
-		tokenCount: Number,
-		color: String
-	},
-	{
-		toObject: { virtuals: true },
-		toJSON: { virtuals: true }
-	}
+  {
+    username: { type: String, required: true, index: { unique: true } },
+    hashedPassword: { type: String, required: true },
+    claimedRouteList: [{ type: Schema.Types.ObjectId, ref: 'Route' }],
+    trainCardHand: [{ type: Schema.Types.ObjectId, ref: 'TrainCard' }],
+    destinationCardHand: [{ type: Schema.Types.ObjectId, ref: 'DestinationCard' }],
+    score: Number,
+    tokenCount: Number,
+    color: String,
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
 );
 
-UserSchema.virtual('points.public').get(function(this: IUserModel) {
-	return this.get('detailed.routes') + this.get('detailed.longestRoute');
-});
+UserSchema.methods.publicPoints = function() {
+  return Promise.all([this.routePoints(), this.longestRoutePoints()]).then(resolved => {
+    //sum all results
+    return resolved.reduce((a, b) => a + b, 0);
+  });
+};
 
-UserSchema.virtual('points.private').get(function(this: IUserModel) {
-	return (
-		this.get('detailed.destinationCardNegative') +
-		this.get('detailed.destinationCardPositive')
-	);
-});
+UserSchema.methods.privatePoints = function() {
+  return Promise.all([this.destinationCardNegativePoints(), this.destinationCardPositivePoints()]).then(resolved => {
+    //sum all results
+    return resolved.reduce((a, b) => a + b, 0);
+  });
+};
 
-UserSchema.virtual('detailed.routes').get(async function(this: IUserModel) {
-	if (!this.claimedRouteList) return 0;
+UserSchema.methods.routePoints = async function() {
+  if (!this.claimedRouteList) return 0;
 
-	let points = 0;
-	let routes = await this.populate('claimedRouteList').execPopulate();
-	for (let i = 0; i < this.claimedRouteList.length; i++) {
-		points += this.claimedRouteList[i].pointValue;
-	}
-	return points;
-});
+  let points = 0;
+  let routes = await this.populate('claimedRouteList').execPopulate();
+  for (let i = 0; i < this.claimedRouteList.length; i++) {
+    points += this.claimedRouteList[i].pointValue;
+  }
+  return points;
+};
 
-UserSchema.virtual('detailed.destinationCardNegative').get(function(
-	this: IUserModel
-) {
-	return 0;
-});
+UserSchema.methods.longestRoutePoints = function() {};
 
-UserSchema.virtual('detailed.destinationCardPositive').get(function(
-	this: IUserModel
-) {
-	return 0;
-});
+UserSchema.methods.destinationCardNegativePoints = function() {};
 
-UserSchema.virtual('detailed.longestRoute').get(function(this: IUserModel) {
-	return 0;
-});
+UserSchema.methods.destinationCardPositivePoints = function() {};
+
+var DestinationCardFulfilled = (routes: IRouteModel[], destinationCard: IDestinationCardModel) => {
+  let visited: string[] = [];
+
+  var traverse = (curCity: string, findCity: string): any => {
+    for (var i = 0; i < routes.length; i++) {
+      if (routes[i].city1 == curCity || (routes[i].city2 == curCity && visited.indexOf(routes[i]._id) === -1)) {
+        visited.push(routes[i]._id);
+        let newCity = routes[i].city1 == curCity ? routes[i].city2 : routes[i].city1;
+
+        if (newCity === findCity || traverse(newCity, findCity)) return true;
+      }
+    }
+    return false;
+  };
+};
 
 UserSchema.virtual('trainCardCount').get(function(this: IUserModel) {
-	let counts = {
-		pink: 0,
-		black: 0,
-		green: 0,
-		blue: 0,
-		white: 0,
-		yellow: 0,
-		orange: 0,
-		red: 0,
-		rainbow: 0
-	};
-	for (let i = 0; i < this.trainCardHand.length; i++) {
-		(counts as any)[this.trainCardHand[i].color]++;
-	}
-	return counts;
+  let counts = {
+    pink: 0,
+    black: 0,
+    green: 0,
+    blue: 0,
+    white: 0,
+    yellow: 0,
+    orange: 0,
+    red: 0,
+    rainbow: 0,
+  };
+  for (let i = 0; i < this.trainCardHand.length; i++) {
+    (counts as any)[this.trainCardHand[i].color]++;
+  }
+  return counts;
 });
 
-export const User: mongoose.Model<IUserModel> = mongoose.model<IUserModel>(
-	'User',
-	UserSchema
-);
+export const User: mongoose.Model<IUserModel> = mongoose.model<IUserModel>('User', UserSchema);
