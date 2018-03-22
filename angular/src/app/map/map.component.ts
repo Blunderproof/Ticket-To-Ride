b8690b233462744966a553fbe1d698fa53eca1a4
Clay Coleman <coleclayman@gmail.com>
2018-03-22 15:11:37 -0600

    edited server_proxy and map component to fire off requests

--- angular/src/app/map/map.component.ts
+++ angular/src/app/map/map.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { GameHistory } from '../services/game-history.service';
 import { UserInfo } from '../services/user_info.service';
+import { ServerProxy } from '../services/server_proxy.service';
 import { User } from '../classes/user';
 import { RouteColor } from '../classes/constants';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
-
   overlayApplied = true;
   trainPathStates = {};
   routeColorEnum = RouteColor;
 
-  constructor(public _gameHistory: GameHistory, public _userInfo: UserInfo) { }
+  constructor(public _gameHistory: GameHistory, public _userInfo: UserInfo, private communicator: ServerProxy) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   mapClicked(event, correspondingTrainPath) {
     const routeInfo = event.path[2].id.split('-');
@@ -27,7 +26,15 @@ export class MapComponent implements OnInit {
     const routeCity1 = routeInfo[2];
     const routeCity2 = routeInfo[3];
 
+    const data = {
+      routeCity1,
+      routeCity2,
+      routeColor,
+      routeNumber,
+    };
+
+    this.communicator.claimRoute(data);
+
     this.trainPathStates[correspondingTrainPath] = this._userInfo.user.color;
   }
-
 }
