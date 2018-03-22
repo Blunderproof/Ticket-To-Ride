7fe776744ac3b192453e78ae0acac8cdcab13595
Jay Davis <jay_s_davis@hotmail.com>
2018-03-22 11:55:00 -0600

    Getting map click events ready to send route information.

--- angular/src/app/map/map.component.ts
+++ angular/src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameHistory } from '../services/game-history.service';
 import { UserInfo } from '../services/user_info.service';
 import { User } from '../classes/user';
+import { RouteColor } from '../classes/constants';
 
 @Component({
   selector: 'app-map',
@@ -12,12 +13,20 @@ export class MapComponent implements OnInit {
 
   overlayApplied = true;
   trainPathStates = {};
+  routeColorEnum = RouteColor;
+
   constructor(public _gameHistory: GameHistory, public _userInfo: UserInfo) { }
 
   ngOnInit() {
   }
 
-  mapClicked(correspondingTrainPath) {
+  mapClicked(event, correspondingTrainPath) {
+    const routeInfo = event.path[2].id.split('-');
+    const routeColor = this.routeColorEnum[routeInfo[0]];
+    const routeNumber = routeInfo[1];
+    const routeCity1 = routeInfo[2];
+    const routeCity2 = routeInfo[3];
+
     this.trainPathStates[correspondingTrainPath] = this._userInfo.user.color;
   }
 
