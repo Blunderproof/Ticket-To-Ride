854e0a2770e4ebfe1fee025b63b6a425db01e5ae
Clay Coleman <coleclayman@gmail.com>
2018-04-06 16:18:49 -0600

    created parent DAO

---
+++ daos/IDAO.ts
@@ -0,0 +1,15 @@
+import IDestinationCardDAO from './IDestinationCardDAO';
+import { IGameDAO } from './IGameDAO';
+import IMessageDAO from './IMessageDAO';
+import IRouteDAO from './IRouteDAO';
+import ITrainCardDAO from './ITrainCardDAO';
+import IUserDAO from './IUserDAO';
+
+export interface IDAO {
+  destinationCardDAO: IDestinationCardDAO;
+  gameDAO: IGameDAO;
+  messageDAO: IMessageDAO;
+  routeDAO: IRouteDAO;
+  trainCardDAO: ITrainCardDAO;
+  userDAO: IUserDAO;
+}
