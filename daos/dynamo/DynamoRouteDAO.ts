dd6b9423c86af6461ff0fc9966db0980c5fc5efb
delashum <delashum@gmail.com>
2018-04-09 16:55:27 -0600

    fix route

--- daos/dynamo/DynamoRouteDAO.ts
+++ daos/dynamo/DynamoRouteDAO.ts
@@ -2,6 +2,9 @@ import IRouteDAO from '../IRouteDAO';
 import { IRouteModel } from '../../models/Route';
 
 export class DynamoRouteDAO implements IRouteDAO {
+  findOne(data: any, populates: any[]): Promise<IRouteModel | null> {
+    throw new Error('Method not implemented.');
+  }
   find(data: any, populates: any[]): Promise<IRouteModel[]> {
     throw new Error('Method not implemented.');
   }
